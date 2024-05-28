3af678383469c7771b2e2249ea335ce1ab48b4c8
emilie-thomas <emiliethomas34@gmail.com>
2024-05-28 11:06:40 +0200

    Revert "Fake commit pour pouvoir revenir en arrière avec un revert"
    This reverts commit af367e9bd7af0203efe7394717153adf37688b1b.

--- assets/router/Route.js
+++ assets/router/Route.js
@@ -1,4 +1,13 @@
-
+/*
+export default class Route {
+  constructor(url, title, pathHtml, pathJS = "") {
+    this.url = url; // url de la page
+    this.title = title; // titre de la page
+    this.pathHtml = pathHtml; // lien vers le fichier HTML
+    this.pathJS = pathJS; // lien vers le fichier JS
+  }
+}
+*/
 export default class Route {
   constructor(url, title, pathHtml, pathJS = "") {
     this.url = url;
--- assets/router/allRoutes.js
+++ assets/router/allRoutes.js
@@ -1,9 +1,20 @@
+/*
+import Route from "./route.js";
 
-import Route from "Route.js";
+// Définir ici nos routes
+export const allRoutes = [
+  new Route("/", "Accueil", "/pages/home.html"),
+];
+
+// Le titre s'affcihe comme ceci : Route.titre - websitename
+export const websiteName = "Quai Antique";
+*/
+
+import Route from "./Route.js";
 
 //Définir ici vos routes
 export const allRoutes = [
-    new Route("/", "Accueil", "../../pages/home.html"),];
+    new Route("/", "Accueil", "/assets/pages/home.html"),];
 
 //Le titre s'affiche comme ceci : Route.titre - websitename
 export const websiteName = "Quai Antique";
