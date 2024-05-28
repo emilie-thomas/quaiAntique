af367e9bd7af0203efe7394717153adf37688b1b
emilie-thomas <emiliethomas34@gmail.com>
2024-05-28 11:05:10 +0200

    Fake commit pour pouvoir revenir en arrière avec un revert

--- assets/router/allRoutes.js
+++ assets/router/allRoutes.js
@@ -1,20 +1,9 @@
-/*
-import Route from "./route.js";
 
-// Définir ici nos routes
-export const allRoutes = [
-  new Route("/", "Accueil", "/pages/home.html"),
-];
-
-// Le titre s'affcihe comme ceci : Route.titre - websitename
-export const websiteName = "Quai Antique";
-*/
-
-import Route from "./Route.js";
+import Route from "Route.js";
 
 //Définir ici vos routes
 export const allRoutes = [
-    new Route("/", "Accueil", "/assets/pages/home.html"),];
+    new Route("/", "Accueil", "../../pages/home.html"),];
 
 //Le titre s'affiche comme ceci : Route.titre - websitename
 export const websiteName = "Quai Antique";
