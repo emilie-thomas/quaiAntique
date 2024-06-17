6a5cb7504a4d5752626785ef4dd38fe97be9f8a5
emilie-thomas <emiliethomas34@gmail.com>
2024-06-17 13:32:30 +0200

    Création pages myAccount et editPassword

--- assets/router/allRoutes.js
+++ assets/router/allRoutes.js
@@ -6,8 +6,10 @@ export const allRoutes = [
     new Route("/", "Accueil", "../pages/home.html"),
     new Route("/galerie", "Galerie photo", "../pages/galerie.html"),
     new Route("/menu", "La carte", "../pages/menu.html" ),
-    new Route("/signIn", "Connexion", "../pages/signIn.html"),
-    new Route('/signUp', "Créer un compte", "../pages/signUp.html"),
+    new Route("/signIn", "Connexion", "../pages/authentification/signIn.html"),
+    new Route('/signUp', "Créer un compte", "../pages/authentification/signUp.html"),
+    new Route("/myAccount", "Mon compte", "../pages/authentification/myAccount.html"),
+    new Route("/editPassword", "Modifier mon mot de passe", "../pages/authentification/editPassword.html"),
 ];
 
 //Le titre s'affiche comme ceci : Route.titre - websitename
