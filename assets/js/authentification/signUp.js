5eb2b309c3b4b60d91532a871b4bb460b7c2278c
emilie-thomas <emiliethomas34@gmail.com>
2024-06-19 12:22:46 +0200

    Formulaire d'incription : dynamisation JS vérification des champs

---
+++ assets/js/authentification/signUp.js
@@ -0,0 +1,105 @@
+const lastName = document.getElementById('lastName');
+const firstName = document.getElementById('firstName');
+const phone = document.getElementById('phone');
+const email = document.getElementById('email');
+const password = document.getElementById('password');
+const passwordRepeat = document.getElementById('passwordRepeat');
+const btnCreateAccount = document.getElementById('btnCreateAccount');
+
+
+lastName.addEventListener('keyup', validateForm);
+firstName.addEventListener('keyup', validateForm);
+phone.addEventListener('keyup', validateForm);
+email.addEventListener('keyup', validateForm);
+password.addEventListener('keyup', validateForm);
+passwordRepeat.addEventListener('keyup', validateForm);
+
+
+// Fonction permettant de valider tout le formulaire :
+btnCreateAccount.disabled = true;
+function validateForm() {
+  const lastNameOK = validateRequired(lastName);
+  const firstNameOK = validateRequired(firstName);
+  const phoneOK = validateRequired(phone);
+  const emailOK = validateEmail(email);
+  const passwordOK = validatePassword(password);
+  const passwordRepeatOK = validateConfirmPassword(password, passwordRepeat);
+
+  if(lastNameOK && firstNameOK && phoneOK && emailOK && passwordOK && passwordRepeatOK) {
+    btnCreateAccount.disabled = false;
+  } else {
+    btnCreateAccount.disabled = true;
+  }
+}
+
+
+
+function validateRequired(input) {
+  if(input.value != ''){
+    input.classList.add('is-valid');
+    input.classList.remove('is-invalid');
+    return true;
+  } else {
+    input.classList.remove('is-valid');
+    input.classList.add('is-invalid');
+    return false;
+  }
+}
+
+function validateEmail(input){
+  //Définir le regex
+  const emailRegex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
+  const userEmail = input.value;
+  if (userEmail.match(emailRegex)) {
+    input.classList.add('is-valid');
+    input.classList.remove('is-invalid');
+    return true;
+  } else {
+    input.classList.remove('is-valid');
+    input.classList.add('is-invalid');
+    return false;
+  }
+}
+
+function validatePassword(input) {
+  const passwordRegex = /^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[\W_])[A-Za-z\d\W_]{8,}$/;
+  const userPassword = input.value;
+  if(userPassword.match(passwordRegex)) {
+    input.classList.add('is-valid');
+    input.classList.remove('is-invalid');
+    return true;
+  } else {
+    input.classList.remove('is-valid');
+    input.classList.add('is-invalid');
+    return false;
+  }
+}
+
+function validateConfirmPassword(inputPassword, inputConfirmPassword) {
+  if(inputPassword.value === inputConfirmPassword.value) {
+    inputConfirmPassword.classList.add('is-valid');
+    inputConfirmPassword.classList.remove('is-invalid');
+    return true;
+  } else {
+    inputConfirmPassword.classList.remove('is-valid');
+    inputConfirmPassword.classList.add('is-invalid');
+    return false;
+  }
+}
+
+//regex pour le téléphone à trouver
+function validatePhone(input) {
+  const phoneRegex = / /;
+  const userPhone = input.value;
+  if(userPhone.match(phoneRegex)) {
+    input.classList.add('is-valid');
+    input.classList.remove('is-invalid');
+    return true;
+  } else {
+    input.classList.remove('is-valid');
+    input.add('is-invalid');
+    return false;
+  }
+}
+
+
--- assets/router/allRoutes.js
+++ assets/router/allRoutes.js
@@ -7,7 +7,7 @@ export const allRoutes = [
     new Route("/galerie", "Galerie photo", "../pages/galerie.html"),
     new Route("/menu", "La carte", "../pages/menu.html" ),
     new Route("/signIn", "Connexion", "../pages/authentification/signIn.html"),
-    new Route('/signUp', "Créer un compte", "../pages/authentification/signUp.html"),
+    new Route('/signUp', "Créer un compte", "../pages/authentification/signUp.html", "../assets/js/authentification/signUp.js"),
     new Route("/myAccount", "Mon compte", "../pages/authentification/myAccount.html"),
     new Route("/editPassword", "Modifier mon mot de passe", "../pages/authentification/editPassword.html"),
     new Route("/manageResa", "Vos Réservations", "../pages/reservations/manageResa.html"),
