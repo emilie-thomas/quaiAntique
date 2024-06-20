86980628e84146648f876773445041121feeff53
emilie-thomas <emiliethomas34@gmail.com>
2024-06-20 10:49:26 +0200

    Fonction connexion : ajout des méthodes gestion token et cookie

--- assets/js/authentification/signIn.js
+++ assets/js/authentification/signIn.js
@@ -1,3 +1,4 @@
+
 // On va stocker la connexion sous forme de cookie => on va stocker un token (jeton d'identification) 
 
 const email = document.getElementById('email');
@@ -11,10 +12,9 @@ function checkCredentials() {
   // IMPORTANT !!! il faudra appeler l'API ici pour vérifier les credentials en BDD :
   
   if(email.value == "test@mail.com" && password.value == "123") {
-    alert('Vous êtes connecté');
-
     // ATTENTION : il faudra placer le vrai token :
     const token = "jknfjnjkvgfn";
+    setToken(token);
     // placer ce token en cookie
 
     window.location.replace("/");
@@ -23,3 +23,12 @@ function checkCredentials() {
     password.classList.add('is-invalid');
   }
 }
+
+// => La méthode checkCredentials() permet de vérifier si on a le bon mail et le bon password. Si la vérification réussit, alors on ajoute un token en cookie (token factice tant que l'on a pas de BDD)
+
+
+/* 
+La connexion = 
+  1- Vérifier les données utilisateurs (identifiant, mdp)
+  2- Stocker un token en cookie pour valider que l'utilisateur est connecté
+*/
